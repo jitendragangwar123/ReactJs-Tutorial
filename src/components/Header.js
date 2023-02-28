aa848980e797e6662c3f9e00d9d2035861b5b6ce
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2023-02-28 13:53:58 +0530

    style variable added

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 const Header = ({title}) => {
   return (
     <div>
-      <h1 style={{color:'red'}}>{title}</h1>
+      <h1 style={headingStyle}>{title}</h1>
     </div>
   )
 }
@@ -13,9 +13,14 @@ Header.defaultProps={
   title:'Task Tracker'
 }
 
+const headingStyle={
+  color:'red',
+}
 // define the props types
 Header.propTypes={
   title: PropTypes.string.isRequired,
 }
+
+
 
 export default Header
