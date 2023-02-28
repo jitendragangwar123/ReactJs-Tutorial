0523b34ff43c982af6139199ef84781f40c7abb0
jitendragangwar123 <gangwarjitendra2498@gmail.com>
2023-02-28 14:42:02 +0530

    Button.js Added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({title}) => {
+  const onClick=()=>{
+    console.log("Click");
+  }
   return (
-    <div>
-      <h1 style={headingStyle}>{title}</h1>
-    </div>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' onClick={onClick}/>
+    </header>
   )
 }
 
@@ -13,9 +18,11 @@ Header.defaultProps={
   title:'Task Tracker'
 }
 
-const headingStyle={
-  color:'red',
-}
+//css in js
+// const headingStyle={
+//   color:'red',
+// }
+
 // define the props types
 Header.propTypes={
   title: PropTypes.string.isRequired,
