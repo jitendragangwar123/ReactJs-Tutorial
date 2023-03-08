885221cd40bafe2e06388ed0d227d035d1df2050
Jitendra Kumar <76531339+jitendragangwar123@users.noreply.github.com>
2023-03-08 23:54:00 +0530

    App. js updated

--- src/App.js
+++ src/App.js
@@ -36,7 +36,8 @@ function App() {
   return (
     <div className="container">
       <Header/>
-      {/*tasks length should be greater than zero*/}
+      {/*tasks length should be greater than zero athorwise 
+No Tasks to Show */}
       {tasks.length>0 ?(
       <Tasks tasks={tasks} onDelete={deleteTask}/>
       ):(
